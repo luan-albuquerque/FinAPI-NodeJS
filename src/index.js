cd8f01ee6c8ba70647954cc23708fdcc61fb88d0
luan-albuquerque <luan.santos6605@gmail.com>
2021-12-01 22:37:58 -0300

    Realizar Saque

--- src/index.js
+++ src/index.js
@@ -24,6 +24,20 @@ function verifyExistsAcountCPG(req,res,next){
 
 } 
 
+function getBalance(statement){
+   
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'Credit'){
+            return acc + operation.amount
+        }else{
+
+             return acc - operation.amount
+        }
+       },0);
+     return balance;
+
+}
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body
     
@@ -73,6 +87,28 @@ app.post("/deposit", verifyExistsAcountCPG,(req,res) =>{
 
       return res.status(201).send();
 })
+
+app.post("/withdraw", verifyExistsAcountCPG, (req,res) =>{
+const { amount } = req.body
+const { customer } = req;
+
+const balance = getBalance(customer.statement)
+
+if(balance < amount){
+    return res.status(400).json({erro: "Insufficient Funds!!"})
+}
+
+const statementOperation = {
+     amount,
+     created_at : new Date(),
+     type : 'debit'
+}
+
+customer.statement.push(statementOperation)
+
+return res.status(200).send();
+
+});
 
 
 app.listen(3333)
